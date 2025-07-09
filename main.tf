13ca67006550c70d7971e489a85a208e2d5db8b2
abheecrazy12 <76834026+abheecrazy12@users.noreply.github.com>
2025-07-09 17:04:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,22 @@
-terraform {
+tterraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"  
+      version = "~> 5.0"
     }
   }
 }
-provider "aws"{
-    region ="us-east-1"
+
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my_bucket"{
-    bucket = "mysimple1234321000"
-    acl = "private"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "mysimple1234321000"
+  # Remove acl from here
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
