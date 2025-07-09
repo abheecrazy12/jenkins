041424defc66be12a952653d8836120791b17108
abhi <abheecrazy12@gmail.com>
2025-07-09 15:42:26 +0530

    done# Please enter the commit message for your changes. Lines starting

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  
+    }
+  }
+}
 provider "aws"{
     region ="us-east-1"
 }
 
 resource "aws_s3_bucket" "my_bucket"{
-    bucket = "mysimple1234321"
+    bucket = "mysimple1234321000"
     acl = "private"
 }
